6580df2280e63a7ede7f27b9f3f9e06783bb6dc6
Ertugrul Söylemez <es@ertes.de>
2013-08-16 17:26:42 +0400

    Event type now opaque to protect time continuity.
    Ignore-this: 6d011944cd4c0d3fc84d70a2f4e6a042
    
    darcs-hash:20130816132642-a5e61-bf81963d86a025fa15d96cf3317edf5f36bdce55

--- Control/Wire/FRP/Event.hs
+++ Control/Wire/FRP/Event.hs
@@ -5,7 +5,23 @@
 -- Maintainer: Ertugrul Soeylemez <es@ertes.de>
 
 module Control.Wire.FRP.Event
-    ( -- * Simple
+    ( -- * Events
+      Event
     )
     where
 
+import Data.Monoid
+
+
+-- | Denotes a stream of values, each together with time of occurrence.
+
+data Event a = Event a | NoEvent
+    deriving (Functor, Show)
+
+instance (Monoid a) => Monoid (Event a) where
+    mempty = NoEvent
+
+    mappend (Event x) (Event y) = Event (x <> y)
+    mappend (Event x) _ = Event x
+    mappend _ (Event y) = Event y
+    mappend _ _         = NoEvent
--- Control/Wire/FRP/Types.hs
+++ Control/Wire/FRP/Types.hs
@@ -8,84 +8,13 @@ module Control.Wire.FRP.Types
     ( -- * Wire synonyms
       Occasion,
       Wire',
-      WireP',
-
-      -- * Events
-      Event(..),
-      event
+      WireP'
     )
     where
 
-import Control.Applicative
 import Control.Wire.State
 import Control.Wire.Wire
-import Data.Data
-import Data.Foldable (Foldable)
-import Data.Monoid
-import Data.String
 import Data.Time.Clock
-import Data.Traversable (Traversable)
-
-
--- | Signals that represent discrete events.
-
-data Event a = Event a | NoEvent
-    deriving (Data, Eq, Foldable, Functor,
-              Ord, Read, Show, Traversable, Typeable)
-
-instance Alternative Event where
-    empty = NoEvent
-
-    Event x <|> _ = Event x
-    _ <|> Event y = Event y
-    _ <|> _       = NoEvent
-
-instance Applicative Event where
-    pure = Event
-
-    Event f <*> Event x = Event (f x)
-    _       <*> _       = NoEvent
-
-instance (Floating a) => Floating (Event a) where
-    (**) = liftA2 (**)
-    acos = fmap acos
-    acosh = fmap acosh
-    asin = fmap asin
-    asinh = fmap asinh
-    atan = fmap atan
-    atanh = fmap atanh
-    cos = fmap cos
-    cosh = fmap cosh
-    exp = fmap exp
-    log = fmap log
-    logBase = liftA2 logBase
-    pi = pure pi
-    sin = fmap sin
-    sinh = fmap sinh
-    sqrt = fmap sqrt
-    tan = fmap tan
-    tanh = fmap tanh
-
-instance (Fractional a) => Fractional (Event a) where
-    (/)   = liftA2 (/)
-    recip = fmap recip
-    fromRational = pure . fromRational
-
-instance (IsString a) => IsString (Event a) where
-    fromString = pure . fromString
-
-instance (Monoid a) => Monoid (Event a) where
-    mempty  = pure mempty
-    mappend = liftA2 mappend
-
-instance (Num a) => Num (Event a) where
-    (+) = liftA2 (+)
-    (-) = liftA2 (-)
-    (*) = liftA2 (*)
-    abs    = fmap abs
-    negate = fmap negate
-    signum = fmap signum
-    fromInteger = pure . fromInteger
 
 
 -- | Occasions are identity-like wires that may inhibit.
@@ -101,10 +30,3 @@ type Wire' = Wire (Timed NominalDiffTime ()) ()
 -- | Simple pure wires.
 
 type WireP' a b = WireP (Timed NominalDiffTime ()) () a b
-
-
--- | Fold an event.
-
-event :: b -> (a -> b) -> Event a -> b
-event _ e (Event x) = e x
-event n _ NoEvent   = n
