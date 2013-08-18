99463fa1b56832e86cf4e25def11621bb4b6b272
Ertugrul Söylemez <es@ertes.de>
2013-08-18 23:29:27 +0400

    Added kSwitch and rSwitch, inspired by Yampa.
    Ignore-this: aa6c3f06d16e08d0b6727ec0d5a63c31
    
    darcs-hash:20130818192927-a5e61-bcdffa3d4a39a37f4635043b76f77f3132058a17

--- Control/Wire/FRP/Switch.hs
+++ Control/Wire/FRP/Switch.hs
@@ -5,14 +5,19 @@
 -- Maintainer: Ertugrul Soeylemez <es@ertes.de>
 
 module Control.Wire.FRP.Switch
-    ( -- * Occasion-based switches
-      (-->),
+    ( -- * Event-based switches
+      switch,
+      kSwitch,
+      rSwitch,
 
-      -- * Event-based switches
-      switch
+      -- * Occasion-based switches
+      (-->)
     )
     where
 
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
 import Control.Wire.FRP.Event.Unsafe
 import Control.Wire.Wire
 import Data.Monoid
@@ -40,8 +45,55 @@ Wire f --> w2' =
 infixr 1 -->
 
 
--- | Start with the given wire.  As soon as its event occurs, switch to
--- the wire in the event's value.
+-- | Intrinsic recurrent continuation switch:  @kSwitch w1 w2@ starts
+-- with @w1@.  Its signal is received by @w2@, which may choose to
+-- switch to a new wire.  Passes the wire we are switching away from
+-- (the continuation wire) to the new wire, such that it may be reused
+-- in it.
+--
+-- Use this switch, if you need to extend a wire over time.
+--
+-- * Inhibits: when the current wire inhibits.  Inhibition of the
+--   switcher wire is ignored.
+--
+-- * Switch: now, keep state.
+
+kSwitch ::
+    (Monad m, Monoid s)
+    => Wire s e m a b
+    -> Wire s e m (a, b) (Event (Wire s e m a b -> Wire s e m a b))
+    -> Wire s e m a b
+kSwitch = loop mempty
+    where
+    loop s' (Wire f) (Wire g) =
+        Wire $ \ds mx' -> do
+            let s = s' <> ds
+            (mx, w1) <- f ds mx'
+            (mev, w2) <- g ds (liftA2 (,) mx' mx)
+            case (mx, mev) of
+              (Right _, Right (Event sw)) ->
+                  stepWire (loop s (sw w1) w2) mempty mx'
+              _ -> return (mx, loop s w1 w2)
+
+
+-- | Extrinsic recurrent switch:  Start with the given wire.  Each time
+-- the input event occurs, switch to the wire it carries.
+--
+-- * Switch: now, restart state.
+
+rSwitch ::
+    (Monad m, Monoid s)
+    => Wire s e m a b
+    -> Wire s e m (a, Event (Wire s e m a b)) b
+rSwitch w' =
+    mkWire $ \ds (x', ev) ->
+        case ev of
+          NoEvent  -> liftM (second rSwitch) (stepWire w' ds (Right x'))
+          Event w1 -> liftM (second rSwitch) (stepWire w1 ds (Right x'))
+
+
+-- | Intrinsic one-time switch:  Start with the given wire.  As soon as
+-- its event occurs, switch to the wire in the event's value.
 --
 -- * Switch: now, restart state.
 
