fbd5bd3940da379cdc49eee241696c59c0adb091
Ertugrul Söylemez <es@ertes.de>
2013-08-24 03:14:47 +0400

    New occasion wires.
    Ignore-this: 54b6b7815b0f568a7c30afd03be7729
    
    darcs-hash:20130823231447-a5e61-15aef2a2b10e4ef8da6402d5d30575b15f8c9de6

--- Control/Wire/FRP/Occasion.hs
+++ Control/Wire/FRP/Occasion.hs
@@ -7,7 +7,14 @@
 module Control.Wire.FRP.Occasion
     ( -- * Time
       after,
-      for
+      for,
+
+      -- * Predicate-based
+      asSoonAs,
+      unless,
+      until,
+      when,
+      while
     )
     where
 
@@ -17,7 +24,7 @@ import Control.Wire.FRP.Types
 import Control.Wire.State
 import Control.Wire.Wire
 import Data.Monoid
-import Prelude hiding ((.), id)
+import Prelude hiding ((.), id, until)
 
 
 -- | After the given interval has passed.
@@ -38,6 +45,25 @@ after t' =
           else (Left mempty, after t)
 
 
+-- | Produce forever as soon as the given predicate holds.
+--
+-- * Depends: now.
+--
+-- * Inhibits: until the given predicate is true.
+
+asSoonAs ::
+    (Monad m, Monoid e, Monoid s)
+    => (a -> Bool)
+    -> Occasion s e m a
+asSoonAs p = loop
+    where
+    loop =
+        mkPure $ \_ x ->
+            if p x
+              then (Right x, id)
+              else (Left mempty, loop)
+
+
 -- | For the given interval.
 --
 -- * Depends: now.
@@ -54,3 +80,71 @@ for t' =
         if t <= 0
           then (Left mempty, empty)
           else (Right x, for t)
+
+
+-- | When the input does not satisfy the given predicate.
+--
+-- * Depends: now.
+--
+-- * Inhibits: when the predicate succeeds.
+
+unless ::
+    (Monad m, Monoid e, Monoid s)
+    => (a -> Bool)  -- ^ Predicate.
+    -> Occasion s e m a
+unless p =
+    mkPure_ $ \_ x ->
+        if p x then Left mempty else Right x
+
+
+-- | Inhibit forever as soon as the given predicate holds.
+--
+-- * Depends: now.
+--
+-- * Inhibits: forever as soon as the given predicate is true.
+
+until ::
+    (Monad m, Monoid e, Monoid s)
+    => (a -> Bool)
+    -> Occasion s e m a
+until p = loop
+    where
+    loop =
+        mkPure $ \_ x ->
+            if p x
+              then (Left mempty, empty)
+              else (Right x, loop)
+
+
+-- | When the input satisfies the given predicate.
+--
+-- * Depends: now.
+--
+-- * Inhibits: when the predicate fails.
+
+when ::
+    (Monad m, Monoid e, Monoid s)
+    => (a -> Bool)  -- ^ Predicate.
+    -> Occasion s e m a
+when p =
+    mkPure_ $ \_ x ->
+        if p x then Right x else Left mempty
+
+
+-- | Inhibit forever as soon as the given predicate fails.
+--
+-- * Depends: now.
+--
+-- * Inhibits: forever as soon as the given predicate is false.
+
+while ::
+    (Monad m, Monoid e, Monoid s)
+    => (a -> Bool)
+    -> Occasion s e m a
+while p = loop
+    where
+    loop =
+        mkPure $ \_ x ->
+            if p x
+              then (Right x, loop)
+              else (Left mempty, empty)
